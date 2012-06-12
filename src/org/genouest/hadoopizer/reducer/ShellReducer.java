ef3e13f6528d516bea65248697c0fc1a4647ac7b
abretaud <abretaud@irisa.fr>
2012-06-12 17:28:09 +0200

    we need a reducer to support multiple outputs

---
+++ src/org/genouest/hadoopizer/reducer/ShellReducer.java
@@ -0,0 +1,34 @@
+package org.genouest.hadoopizer.reducer;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.ObjectWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.genouest.hadoopizer.io.ObjectWritableComparable;
+
+public class ShellReducer extends Reducer<ObjectWritableComparable, ObjectWritable, ObjectWritableComparable, ObjectWritable> {
+    
+    private MultipleOutputs<ObjectWritableComparable, ObjectWritable> mos;
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        
+        mos = new MultipleOutputs<ObjectWritableComparable, ObjectWritable>(context);
+    }
+
+    @Override
+    protected void reduce(ObjectWritableComparable key, Iterable<ObjectWritable> values, Context context) throws IOException, InterruptedException {
+        
+        // TODO comment
+        for (ObjectWritable valuein : values) {
+            mos.write(key.getKeyType(), key, valuein);
+        }
+    }
+    
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        
+        mos.close();
+    }
+}
